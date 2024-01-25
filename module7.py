db2d28626aefd276006aa200b52bd276ff08818b
gumina-csuglobal <joshua.gumina@csuglobal.edu>
2024-01-24 22:21:54 -0700

    Initial requirements

---
+++ module7.py
@@ -0,0 +1,55 @@
+# inititial data containing room numbers for courses
+# Key: Course number, Value: Room Number
+course_rooms = {
+    "CSC101": "3004",
+    "CSC102": "4501",
+    "CSC103": "6755",
+    "NET110": "1244",
+    "COM241": "1411"
+}
+
+# inititial data containing instructors for courses
+# Key: Course number, Value: Instructor
+course_instructors = {
+    "CSC101": "Haynes",
+    "CSC102": "Alvarado",
+    "CSC103": "Rich",
+    "NET110": "Burke",
+    "COM241": "Lee"
+}
+
+# inititial data containing course times for courses
+# Key: Course number, Value: Course time
+course_times = {
+    "CSC101": "8:00 a.m.",
+    "CSC102": "9:00 a.m.",
+    "CSC103": "10:00 a.m.",
+    "NET110": "11:00 a.m.",
+    "COM241": "1:00 p.m."
+}
+
+# Returns tuple of room#, instructor, time for a given course_number
+def get_course_info(course_number)
+    return (
+        course_rooms(course_number),
+        course_instructors(course_number),
+        course_times(course_number),
+    )
+
+def main():
+    course_number = input("Please enter course number to view: ")
+    try:
+        room, instructor, time = get_course_info(course_number)
+        print(f"\nCourse Information for course {course_number}")
+        print(f"Room Number: {room}")
+        print(f"Room Number: {instructor}")
+        print(f"Room Number: {time}")
+
+    except:
+        print(f"You entered an invalid or non-existant course number ({course_number}).")
+    
+   
+
+if __name__ == "__main__":
+    main()
+
